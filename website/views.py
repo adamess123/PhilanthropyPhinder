be717edab2277ffd3222df74be4ca3f30286db78
adamess123 <adamess123@gmail.com>
2023-02-23 12:39:20 -0500

    Added handling for resetting password

--- website/views.py
+++ website/views.py
@@ -1,8 +1,6 @@
 from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
-from .models import Note
 from . import db
-import json
 
 views = Blueprint('views', __name__)
 
@@ -20,16 +18,4 @@ def home():
             db.session.commit()
             flash('Note added.', category='success')
 
-    return render_template("home.html", user=current_user)
-
-@views.route('/delete-note', methods=['POST'])
-def delete_note():
-    note = json.loads(request.data)
-    noteId = note['note']
-    note = Note.query.get(noteId)
-    if note:
-        if note.user_id == current_user.id:
-            db.session.delete(note)
-            db.session.commit()
-
-    return jsonify({})
+    return render_template("index.html", user=current_user)
